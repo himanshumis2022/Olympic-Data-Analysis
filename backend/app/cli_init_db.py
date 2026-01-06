f88200fda6eb79066444765d3c953da5ea436dfa
Himanshu Mishra <160376994+himanshumis2022@users.noreply.github.com>
2026-01-06 12:42:20 +0530

    Delete backend directory

--- backend/app/cli_init_db.py
+++
@@ -1,8 +0,0 @@
-from .startup import init_db
-
-def main():
-	init_db()
-	print("Database initialized.")
-
-if __name__ == "__main__":
-	main()
--- backend/app/startup.py
+++
@@ -1,4 +0,0 @@
-from .db import Base, engine
-
-def init_db() -> None:
-	Base.metadata.create_all(bind=engine)
